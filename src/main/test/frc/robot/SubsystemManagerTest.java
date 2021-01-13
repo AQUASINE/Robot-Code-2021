de7ff0a2b747b96f647b80ad04b79c98d689596d
2102062 <2102062@geneva304.org>
2021-01-13 14:53:57 -0600

    Imported mockito correctly, got base test file set up
    Also created SubsystemManager file

---
+++ src/main/test/frc/robot/SubsystemManagerTest.java
@@ -0,0 +1,15 @@
+package frc.robot;
+
+import org.junit.Rule;
+import org.junit.runner.RunWith;
+import org.mockito.junit.MockitoJUnit;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.mockito.junit.MockitoRule;
+
+@RunWith(MockitoJUnitRunner.class)
+public class SubsystemManagerTest {
+    @Rule
+    public MockitoRule initRule = MockitoJUnit.rule();
+
+
+}
