1ff1f38cb30052fad705c36c853ec93150e93867
Coding MAIN <robovikes3067@gmail.com>
2021-02-03 15:34:48 -0600

    Imported Gyro and added preliminary gyro code

--- src/main/java/frc/robot/command/auto/RotateCommand.java
+++ src/main/java/frc/robot/command/auto/RotateCommand.java
@@ -5,6 +5,9 @@ import frc.robot.subsystem.DriveImpl;
 
 public class RotateCommand extends Command {
     private DriveImpl drive;
+    private double startingAngle = 0;
+    private double targetAngle;
+    private double relativeAngle;
 
     public RotateCommand(DriveImpl drive) {
         this.drive = drive;
@@ -12,13 +15,20 @@ public class RotateCommand extends Command {
     }
 
     @Override
+    protected void initialize() {
+        startingAngle = drive.gyro.getAngle();
+        targetAngle = 180;
+    }
+
+    @Override
     protected void execute() {
+        relativeAngle = drive.gyro.getAngle() - startingAngle;
         drive.m_right.set(.1);
         drive.m_left.set(-.1);
     }
 
     @Override
     protected boolean isFinished() {
-        return false;
+        return relativeAngle >= targetAngle;
     }
 }
