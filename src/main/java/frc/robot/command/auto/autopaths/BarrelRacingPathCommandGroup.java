4bec2a933274953376d09f831af3d63b947331f0
Coding MAIN <robovikes3067@gmail.com>
2021-03-03 18:01:57 -0600

    Finished barrel racing path

--- src/main/java/frc/robot/command/auto/autopaths/BarrelRacingPathCommandGroup.java
+++ src/main/java/frc/robot/command/auto/autopaths/BarrelRacingPathCommandGroup.java
@@ -11,27 +11,33 @@ public class BarrelRacingPathCommandGroup extends SequentialCommandGroup {
     public BarrelRacingPathCommandGroup(DriveSubsystem drive) {
         addCommands(
                 new DriveLengthConstantCommand(150, drive),
-                new RotateConstantCommand(87, drive),
+                //first turn start
+                new RotateConstantCommand(86, drive),
                 new DriveLengthConstantCommand(70, drive),
-                new RotateConstantCommand(88, drive),
+                new RotateConstantCommand(86, drive),
                 new DriveLengthConstantCommand(60, drive),
-                new RotateConstantCommand(88, drive),
+                new RotateConstantCommand(86, drive),
                 new DriveLengthConstantCommand(70, drive),
-                new RotateConstantCommand(88, drive),
-                new DriveLengthConstantCommand(170, drive),
+                new RotateConstantCommand(86, drive),
+                //first turn end
+                new DriveLengthConstantCommand(158, drive),
+                //start of second turn
                 new RotateConstantCommand(-85, drive),
                 new DriveLengthConstantCommand(60, drive),
                 new RotateConstantCommand(-85, drive),
                 new DriveLengthConstantCommand(60, drive),
                 new RotateConstantCommand(-85, drive),
-                new DriveLengthConstantCommand(80, drive),
+                new DriveLengthConstantCommand(70, drive),
                 new RotateConstantCommand(-45, drive),
+                // end of second turn
                 new DriveLengthConstantCommand(80, drive),
+                //start of third turn
                 new RotateConstantCommand(-45, drive),
                 new DriveLengthConstantCommand(80, drive),
                 new RotateConstantCommand(-90, drive),
-                new DriveLengthConstantCommand(60, drive),
+                new DriveLengthConstantCommand(72, drive),
                 new RotateConstantCommand(-90, drive),
+                //end of third turn
                 new DriveLengthConstantCommand(280, drive)
         );
     }
