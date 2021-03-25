7e742264ef322f7bee95bfbbfba99c0f823f530b
Sean <2102062@geneva304.org>
2021-03-25 15:46:25 -0500

    feat: add Intake Subsystem

---
+++ src/main/java/frc/robot/subsystem/IntakeSubsystem.java
@@ -0,0 +1,35 @@
+package frc.robot.subsystem;
+
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
+
+import edu.wpi.first.wpilibj.Solenoid;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+public class IntakeSubsystem extends SubsystemBase {
+    private Solenoid pushSolenoidL;
+    private Solenoid pushSolenoidR;
+    private Solenoid lockSolenoid;
+    private VictorSPX rollerMotor;
+    
+    public IntakeSubsystem() {
+        lockSolenoid = new Solenoid(0);
+        pushSolenoidL = new Solenoid(1);
+        pushSolenoidR = new Solenoid(2);
+        rollerMotor = new VictorSPX(2);
+    }
+
+    public void setPushSolenoids(boolean on) {
+        pushSolenoidL.set(on);
+        pushSolenoidR.set(on);
+    }
+
+    public void setLockSolenoid(boolean on) {
+        lockSolenoid.set(on);
+    }
+
+    public void setRollerMotor(double value) {
+        rollerMotor.set(ControlMode.PercentOutput, value);
+    }
+}
